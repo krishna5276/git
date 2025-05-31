03f27cc5107784128c6c7477db55ad6768ccc7d4
Murali <muralikrishnasettypalli0@gmail.com>
2025-05-31 21:32:37 +0530

    airthamatic

---
+++ operators.c
@@ -0,0 +1,13 @@
+//program 1 Arithamatic
+#include<stdio.h>
+int main()
+{
+int a=10,b=20;
+printf("Airthamatic %d\n",a+b);
+printf("subtraction %d\n",a-b);
+printf("Multiplication %d\n",a*b);
+printf("Division %d\n",a/b);
+printf("Modules %d",a%b);
+
+return 0;
+}
